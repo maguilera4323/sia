e8b11d984749ba68f558c37330cf988499c02c3f
nuevoRON <hronaldjosue@gmail.com>
2022-09-28 21:53:41 -0600

    reunion

--- seguridad/procedimientos.php
+++ seguridad/procedimientos.php
@@ -4,7 +4,7 @@ require_once('conex.php');
 $mysql = new connection();
 $conexion = $mysql-> get_connection();
 
-$datos = array("nom_usu" => "juan", "correo" => "JOH@gmail.com", "clave"=> sha1("efran"), "nombre" =>  "juanchi", "apellido"=> "flores");
+$datos = array("nom_usu" => "pedro", "correo" => "pep@gmail.com", "clave"=> sha1("roberto45"), "nombre" =>  "huhuh", "apellido"=> "flores");
 $statement =$conexion->prepare ("call insert_usu(?,?,?,?,?)");
 $statement->bind_param("sssss", $datos["nom_usu"], $datos["correo"], $datos["clave"], $datos["nombre"], $datos["apellido"]);
 $statement-> execute();
